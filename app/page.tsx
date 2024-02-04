d013df267f4370027625d83f3a7691446a48993b
Huacheng Gao <huachenggao@Huachengs-MacBook-Pro.local>
2024-02-04 10:30:06 -0500

    add a link to subpage, setup baseline service worker

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+// import { useEffect } from "react";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
+  // useEffect(() => {
+  //   if ('serviceWorker' in navigator) {
+  //     navigator.serviceWorker
+  //       .register('/service-worker.js')
+  //       .then(() => console.log('registered service worker'))
+  //   }
+  // }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-20">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -40,11 +51,9 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/arriving"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Arriving{" "}
@@ -55,7 +64,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             We&apos;ll help you navigate into the city
           </p>
-        </a>
+        </Link>
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
