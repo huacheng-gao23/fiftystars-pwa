2a9d5c0dd3f720bd83803d8be50c099cb8de6a0d
Huacheng Gao <huachenggao@Huachengs-MacBook-Pro.local>
2024-02-04 11:26:43 -0500

    abtract navpage

--- app/arriving/page.tsx
+++ app/arriving/page.tsx
@@ -1,5 +1,27 @@
-export default function ArrivingPage() {
+"use client";
+import NavPage from "components/NavPage";
+
+const NavButtonInfo = [
+  {
+    header: "Airport",
+    subheader: "Landing at IAD or DCA?",
+    link: "/arriving"
+  },
+  {
+    header: "Train",
+    subheader: "Arriving at Union Station?",
+    link: "/arriving"
+  },
+  {
+    header: "Car",
+    subheader: "Looking for a place to park?",
+    link: "/arriving"
+  }
+];
+
+export default function Home() {
+
   return (
-    <h2>Hello</h2>
+    <NavPage navButtonArray={NavButtonInfo}></NavPage>
   );
 }
---
+++ components/NavPage.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+interface NavButton {
+  header: string,
+  subheader: string,
+  link: string,
+}
+
+export default function NavPage({navButtonArray}: {navButtonArray: NavButton[]}) {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        {navButtonArray.map(navButton => (
+          <Link
+            href={navButton.link}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            key={navButton.header}
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              {navButton.header}{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              {navButton.subheader}
+            </p>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
